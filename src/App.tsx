6b11094e43d63498d344b38042410568ec03d78d
lucaaramos <lucaramos99@gmail.com>
2023-01-24 02:43:07 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import {BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Routes>
+      <Route path='/pokemons/:items' element={<Items/>}/>
+      <Route path='/pokemons' element={<Pokemons/>}/>
+      <Route path='/pokemons/:name' element={<Pokemons/>}/>  
+      <Route path='/' element={<Pokemons/>}/>
+      </Routes>
       <header className="App-header">
         <h1>
           Hola
         </h1>
       </header>
     </div>
+    </Router>
   );
 }
 

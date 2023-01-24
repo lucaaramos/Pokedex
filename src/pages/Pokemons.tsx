39ad00301a79396c6e34aa72a2d9148ad4fa3c61
lucaaramos <lucaramos99@gmail.com>
2023-01-24 16:53:05 -0300

    fetch

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,14 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-
+import BulbasaurPic from '../assets/bulbasaur.gif'
+import styles from './Pokemons.module.css'
+import { fetchPokemons } from '../api/fetchPokemons';
+import { Pokemon } from '../types/types';
 export default function Pokemons() {
   const [query, setQuery] = useState("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchAllPokemons = async() =>{
+      const allPokemons = await fetchPokemons()
+      
+      setPokemons(allPokemons)
+    };
+    fetchAllPokemons();
+  },[])
+  
   return (
     <>
     <Header query ={query} setQuery={setQuery}/>
     <main>
-        poke
+        <nav className={styles.nav}>
+          {pokemons?.slice(0,151).map((pokemon) => (
+            <Link key={pokemon.id} className={styles.listItems} to={`pokemons/${pokemon.name.toLowerCase}`}>
+          <img
+          className={styles.listitemsIcon}
+          src={pokemon.imgSrc} 
+          alt={pokemon.name}
+          width={50}>
+          
+          </img>
+          <div className={styles.listItemsText}>
+            <span>{pokemon.name}</span>
+            <span>{pokemon.id}</span>
+          </div>
+          </Link>
+          ))}
+                    
+        </nav>
     </main>
     <Footer/>
     </>
